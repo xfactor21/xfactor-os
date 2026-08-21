8a9ffec3310a7cb1608c2af2d5d5e0563163024a
xFactor <xfactorxai@gmail.com>
2026-08-21 13:49:03 -0500

    Activate Incident click-to-dossier UX

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './xfactor/brand-refresh.css'
+import { installIncidentUx } from './xfactor/incidentUx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
+
+installIncidentUx()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
