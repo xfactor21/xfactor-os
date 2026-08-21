ab97146c5dc209579aa5d81bcd9c837f6074899a
xFactor <xfactorxai@gmail.com>
2026-08-21 13:10:09 -0500

    Load xFactor pink cyan brand refresh

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './xfactor/brand-refresh.css'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
