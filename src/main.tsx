e60554360865f381448e2df32a19c24004034934
xFactor <xfactorxai@gmail.com>
2026-08-25 00:18:55 -0500

    Mount first-run tutorial at application boot

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './xfactor/brand-refresh.css'
+import './xfactor/tutorial.css'
 import './modules/studio/draw/draw-pro.css'
 import { installIncidentUx } from './xfactor/incidentUx'
 import { installIncidentContextUx } from './xfactor/incidentContextUx'
 import { installDrawProEnhancements } from './modules/studio/draw/drawProEnhancements'
+import { installXfactorTutorial } from './xfactor/tutorial'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
 
 installIncidentUx()
 installIncidentContextUx()
 installDrawProEnhancements()
+installXfactorTutorial()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
